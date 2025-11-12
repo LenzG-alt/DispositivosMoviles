42ec28b0dbad49b984c72d64386f26982bced54a
Davis <darapac@ulasalle.edu.pe>
2025-11-12 14:31:42 -0500

    agregar filtro de usuarios activos

--- FLUTTER/actividad_en_clase_2_flutter_provider_crud/lib/viewmodels/user_view_model.dart
+++ FLUTTER/actividad_en_clase_2_flutter_provider_crud/lib/viewmodels/user_view_model.dart
@@ -3,8 +3,16 @@ import '../models/user.dart';
 
 class UserViewModel extends ChangeNotifier {
   final List<User> _usuarios = [];
+  bool _mostrarSoloActivos = false;
 
-  List<User> get usuarios => _usuarios;
+  List<User> get usuarios =>
+      _mostrarSoloActivos ? _usuarios.where((u) => u.activo).toList() : _usuarios;
+  bool get mostrarSoloActivos => _mostrarSoloActivos;
+
+  void filtroActivos(bool value) {
+    _mostrarSoloActivos = value;
+    notifyListeners();
+  }
 
   void agregarUsuario(User usuario) {
     _usuarios.add(usuario);
